eb21ca0bd41775c7e1bce408ae028c10ad59ab2c
Ryan Durrant <rddurrant@gmail.com>
2017-03-07 20:32:49 -0700

    pre black diamond

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+/*jshint esversion: 6 */
 
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var app = express();
+app.use(express.static('assets'));
+app.use(bodyParser.json());
+app.listen(3000, () => console.log("All good, captain"));
+
+var messages = [];
+
+app.get('/messages', function(req, res, next){
+  res.status(200).json({messages: messages});
+});
+
+app.post('/messages', function(req, res, next){
+  console.log(req.body);
+  messages.push({ message: req.body.message, time: new Date() });
+  res.status(200).json({messages: messages});
+});
